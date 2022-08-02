7aa00932cc663686a2934dff0a1c680e6e4509b1
RokiLord <youht7290@gmail.com>
2022-08-02 22:04:11 +0900

    function in templates

--- main.go
+++ main.go
@@ -2,27 +2,34 @@ package main
 
 import (
 	"html/template"
-	"log"
-	"os"
+	"strings"
 )
 
-var tpl *template.Template
+var fm = template.FuncMap{
+	"uc": strings.ToUpper,
+	"ft": firstThree,
+}
 
 func init() {
-	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
+	tpl = template.Must(template.New("").Funcs(fm).ParseFiles("tpl.gohtml"))
+}
+
+func firstThree(s string) string {
+	s = strings.TrimSpace(s)
+	s = s[:3]
+	return s
 }
 
 func main() {
 
-	sages := map[string]string{
-		"India":   "Gandhi",
-		"America": "MLK",
+	b := sage{
+		Name:  "Buddha",
+		Motto: "The belief of no beliefs",
 	}
 
-	err := tpl.Execute(os.Stdout, sages)
-
-	if err != nil {
-		log.Fatal(err)
+	g := sage{
+		Name:  "Gandhi",
+		Motto: "Be the change",
 	}
 
 }
